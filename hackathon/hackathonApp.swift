356b0391336e7167017d77f89997fe6438257a59
nush20 <anushka.nd02@gmail.com>
2026-01-02 22:23:32 +0530

    Initial Commit

---
+++ hackathon/hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackathonApp.swift
+//  hackathon
+//
+//  Created by Anushka on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
